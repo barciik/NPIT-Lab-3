49664ea9664c54a33a022bd3973348c1cbc29e6f
Bartek <bartek.batko3@gmail.com>
2023-05-07 12:49:43 +0200

    --log

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,14 @@
 if [[ "$1" == "--date" ]]; then
 	echo "$(date)"
 fi
+
+if [[ "$1" == "--logs" ]]; then
+	for ((i=1; i<=100; i++)); do
+    		filename="log${i}.txt"
+   		echo "Nazwa pliku: ${filename}" > "${filename}"
+    		echo "Nazwa skryptu: ${0}" >> "${filename}"
+    		echo "Data utworzenia: $(date)" >> "${filename}"
+    		echo "Plik ${filename} utworzony."
+  	done
+
+fi
